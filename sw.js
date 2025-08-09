26d5631d6d926900dd5660db8fd9cdc16e65c1d9
RHine. <bakr300max@gmail.com>
2025-08-09 18:17:44 +0300

    👇

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // js/sw.js
-const CACHE_NAME = "workshop-v5.3"; // IMPORTANT: Remember to bump this version number with every new deployment
+const CACHE_NAME = "workshop-v5.6"; // IMPORTANT: Remember to bump this version number with every new deployment
 const URLS_TO_CACHE = [
   "/",
   "/index.html",
